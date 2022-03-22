0522cde072f7882a72c8c6c94e47cb1db3474e24
Arseniy Chertov <60727260+ArseniyChertov@users.noreply.github.com>
2022-03-21 22:34:56 -0400

    Add files via upload

---
+++ cnn_2.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dropout, Flatten, Dense, MaxPooling2D, Conv2D
+
+
+class config():
+    epochs = 10
+    batch_size = 1024
+    workers = 1
+    optimizer = "adam"
+    loss = "categorical_crossentropy"
+
+
+class mnist_cnn():
+    def initialize(hp):
+        input = Input(shape=(28, 28, 1))
+        first_dropout = hp.Choice('first_dropout', [0.0, 0.2, 0.4])
+        x = input
+        x = Dropout(first_dropout)(x)
+
+        network_depth = hp.Choice('network_depth', [1, 2, 3, 4])
+        init_n_flters = hp.Choice('n_filters', [4, 8, 16])
+        n_filters_growth_factor = hp.Choice('n_filters_growth_factor', [2, 4, 8])
+
+        x = Conv2D(init_n_flters, 3, strides=(1, 1), activation='relu', padding='valid')(x)
+
+        for i in range(0, network_depth):
+            x = Conv2D(16, 3, strides=(1, 1), activation='relu', padding='valid')(x)
+
+        for i in range(0, network_depth):
+            depth = (n_filters_growth_factor ** i) * init_n_flters
+            x = Conv2D(depth, 3, strides=(1, 1), activation='relu', padding='valid')(x)
+            conv_dropout_k = hp.Choice('conv_dropout_' + str(i), [0.0, 0.25, 0.5])
+            x = Dropout(conv_dropout_k)(x)
+
+        kernel_size = hp.Choice('units_kernel1', [3, 5, 7])
+        x = Conv2D(hp.Choice('units_conv1', [8, 16, 32]), (kernel_size, kernel_size), strides=(1, 1),
+                   padding=hp.Choice('padding', ['same', 'valid']),
+                   activation=hp.Choice('activation_conv1', ['relu', 'tanh']))(input)
+
+        x = Flatten()(x)
+
+        units_dense = hp.Choice('units_dense', [4, 8, 16, 32])
+        x = Dense(units_dense)(x)
+
+        second_dropout = hp.Choice('second_dropout', [0.0, 0.2, 0.4, 0.6])
+        x = Dropout(second_dropout)(x)
+
+        x = Dense(10, activation="softmax")(x)
+
+        model = Model(inputs=input, outputs=x)
+
+        learning_rate = hp.Float("lr", min_value=1e-4, max_value=1e-2, sampling="log")
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), loss=config.loss,
+                      metrics=['accuracy'])
+
+        return model
+
---
+++ train_hypermodel.py
@@ -0,0 +1,163 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import importlib
+import keras_tuner as kt
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dropout, Flatten, Dense, MaxPooling2D, Conv2D
+
+from sklearn.model_selection import train_test_split, KFold
+
+import sys
+import os
+
+if len(sys.argv) != 2:
+    print("Missing model path")
+    exit()
+
+model_path = sys.argv[-1]
+
+if not os.path.exists(model_path):
+    print("Model path does not exist")
+    exit()
+
+
+def obtain_model(model_path):
+    try:
+        model = getattr(importlib.import_module(model_path.replace('/', '.').replace(".py", "")), "mnist_cnn")
+        config = getattr(importlib.import_module(model_path.replace('/', '.').replace(".py", "")), "config")
+
+        return model, config
+    except ModuleNotFoundError:
+        print("Model path does not exist")
+        exit()
+
+
+# Obtain the data from the mnist set
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Convert ground-truth to categorical labels
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Recombine into one set for splitting via KFold
+X = np.vstack([X_train, X_test])
+y = np.vstack([y_train, y_test])
+
+# Normalize the data
+X = X / 255
+
+# We'll use array indices to distinguish between folds. For now, it's the simplest to read and code.
+folds = 10
+amount_per_fold = len(X) / folds
+
+fold_indices = []
+for i in range(0, folds):
+    fold_indices.append((int(i * amount_per_fold), int((i + 1) * amount_per_fold - 1)))
+
+# Now, fold_indices contains the range of each fold
+# For example, fold_indices[3] is the fourth fold.
+
+test_accuracies = []
+for i in range(0, folds):
+    print("Determining validation set...")
+    valid_folds = set(range(0, folds)) - set([i])
+    validation_fold = np.random.randint(0, len(valid_folds))
+
+    print("Building training set...")
+    valid_folds -= set([validation_fold])
+    valid_folds = list(valid_folds)
+
+    X_train = None
+    y_train = None
+    for j in valid_folds:
+        start, end = fold_indices[j]
+
+        if X_train is None and y_train is None:
+            X_train = np.array(X[start: end + 1])
+            y_train = np.array(y[start: end + 1])
+        else:
+            X_train = np.vstack([X_train, X[start: end + 1]])
+            y_train = np.vstack([y_train, y[start: end + 1]])
+
+    # Now, both X_train and y_train have size (folds - 2) * amount_per_fold
+
+    print("Preparing validation set...")
+    start, end = fold_indices[validation_fold]
+    X_val = np.array(X[start: end + 1])
+    y_val = np.array(y[start: end + 1])
+
+    print("Preparing test set...")
+    start, end = fold_indices[i]
+    X_test = np.array(X[start: end + 1])
+    y_test = np.array(y[start: end + 1])
+
+    print("Initializing model %d..." % (i + 1))
+    model, config = obtain_model(model_path)
+
+    tuner = kt.RandomSearch(
+        model.initialize,
+        objective='val_loss',
+        max_trials=3
+    )
+
+    tuner.search(X_train, y_train, epochs=10, validation_data=(X_val, y_val))
+
+    model = tuner.get_best_models()[0]
+
+    # best_hyperparameters = tuner.get_best_hyperparameters()[0]
+    # model = model.initialize(best_hyperparameters)
+    print(model.summary())
+    # print(tuner.results_summary())
+    exit()
+
+    '''
+    history = model.fit(
+            X_train,
+            y_train,
+            validation_data = (X_val, y_val),
+            epochs = config.epochs,
+            batch_size = config.batch_size,
+            workers = config.workers,
+    ).history
+    '''
+
+    test_accuracies.append(model.evaluate(X_test, y_test)[1])
+    print("*** This fold gave an accuracy of ", test_accuracies[-1])
+    plt.plot(history['accuracy'], color='blue')
+    plt.plot(history['val_accuracy'], color='red')
+
+'''
+print("Average model accuracy: ", np.average(test_accuracies))
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.xticks(range(0, config.epochs, int(config.epochs / 10)))
+plt.gca().legend(("Training", "Validation"))
+plt.show()
+'''
+
+# Notes:
+# TODO
+#   Better graphs?
+#   Confusion Matrix could be cool, might not work here
+
+# TO TEST
+# Validation sets? Idk
+# Batch size
+# Number of filters
+# Presence of max pooling
+# Normalizing input data
+# More convolution layers
+# Bigger filters
+# Strides
+# Kernel sizes (Bigger conv layers)
+# BatchNorm
+# More denses
+# Purely convolution network (no flatten)
+# Activation functions
+# padding
+# Epochs
+# workers? (In fit)
